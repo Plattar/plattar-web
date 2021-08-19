b0051e400658b9351176a5cec40e2674e77cd155
David Arayan <david.arayan12@gmail.com>
2021-08-19 13:53:40 +1000

    - Added ProductElement and <plattar-product> node

---
+++ plattar-web/elements/product-element.js
@@ -0,0 +1,31 @@
+const BaseElement = require("./base/base-element.js");
+
+class ProductElement extends BaseElement {
+    constructor() {
+        super();
+    }
+
+    get permissions() {
+        return ["autoplay"];
+    }
+
+    get elementType() {
+        return "product";
+    }
+
+    get coreAttributes() {
+        return [{
+            key: "product-id",
+            map: "product_id"
+        }];
+    }
+
+    get optionalAttributes() {
+        return [{
+            key: "variation-id",
+            map: "variation_id"
+        }];
+    }
+}
+
+module.exports = ProductElement;
--- plattar-web/index.js
+++ plattar-web/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const WebXRElement = require("./elements/webxr-element.js");
 const ViewerElement = require("./elements/viewer-element.js");
+const ProductElement = require("./elements/product-element.js");
 const EWallElement = require("./elements/ewall-element.js");
 const FaceARElement = require("./elements/facear-element.js");
 const EditorElement = require("./elements/editor-element.js");
@@ -9,6 +10,7 @@ const Version = require("./version");
 
 customElements.define("plattar-webxr", WebXRElement);
 customElements.define("plattar-viewer", ViewerElement);
+customElements.define("plattar-product", ProductElement);
 customElements.define("plattar-editor", EditorElement);
 customElements.define("plattar-facear", FaceARElement);
 customElements.define("plattar-8wall", EWallElement);
