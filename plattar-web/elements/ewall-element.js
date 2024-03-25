5fb460029dbae23a70644baef0debca97a9e23c2
David Arayan <david.arayan12@gmail.com>
2024-03-25 18:13:33 +0400

    - Updated e-wall permissions

--- plattar-web/elements/ewall-element.js
+++ plattar-web/elements/ewall-element.js
@@ -3,29 +3,10 @@ const BaseElement = require("./base/base-element.js");
 class EWallElement extends BaseElement {
     constructor() {
         super();
-
-        const tag = document.createElement("script");
-        tag.src = "https://cdn.8thwall.com/web/iframe/iframe.js";
-        tag.defer = true;
-    }
-
-    connectedCallback() {
-        super.connectedCallback();
-
-        // this is the iframe ID
-        const id = this.element.controller.id;
-
-        const onLoad = () => {
-            if (window.XRIFrame) {
-                window.XRIFrame.registerXRIFrame(id);
-            }
-        }
-
-        window.addEventListener("load", onLoad, false)
     }
 
     get permissions() {
-        return ["camera", "autoplay", "xr-spatial-tracking", "gyroscope", "accelerometer"];
+        return ["camera *", "autoplay *", "xr-spatial-tracking *", "gyroscope *", "accelerometer *", "magnetometer *"];
     }
 
     get elementType() {
